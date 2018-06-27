0448505a048a777562facbb5412930c4d47d0986
Henry T <henryt.inc@gmail.com>
2018-06-27 16:27:50 -0400

    Cleaned PHP

---
+++ php/emailSub.php
@@ -0,0 +1,33 @@
+<?php
+include('MailChimp.php');
+use \DrewM\MailChimp\MailChimp;
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $MailChimp = new MailChimp('apikeygoeshere');
+
+        $list_id = '';
+
+        $subscriber_hash = $MailChimp->subscriberHash($email);
+        $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
+        if($result['status'] == '404') {
+
+            $result = $MailChimp->post("lists/$list_id/members", [
+                'email_address' => $email,
+                'status' => 'subscribed',
+            ]);
+
+            if ($MailChimp->success()) {
+                echo 'success';
+            } else {
+                echo $MailChimp->getLastError();
+            }
+        } else {
+            echo 'asubbed';
+        }
+    } else {
+        echo 'invalid';
+    }
+}
+?>
